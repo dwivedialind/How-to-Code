f8ef69c3c1d0d61092c6c85a9c0f74f3b96341f7
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2022-12-16 13:08:17 +0530

    first day BSL

---
+++ pb1.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname pb1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;Expression are evaluated
+;(+ 3 4)
+;(+ 3 (* 8 7))
+(sqr 3)
+(sqrt 16)
+;calculating hypotonuese having sides 3 and 4 in right angled triangle
+(sqrt (+ (sqr 3) (sqr 4)))
+;this is how irrational numbers are shown
+(sqrt 3)
